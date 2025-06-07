88a3e6eb7ca9dcb2fd143a23a428f9424690b76d
niclas.behrendt <niclas.behrendt@tallence.com>
2025-06-07 16:25:27 +0200

    move providers to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0"
+    }
+  }
+}
+
 resource "google_compute_network" "vpc_network" {
   name                    = "iac-vpc"
   auto_create_subnetworks = false
