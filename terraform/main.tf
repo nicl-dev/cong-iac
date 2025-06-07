ee37438b93f874e953eed9038c5f54dbb21828c6
niclas.behrendt <niclas.behrendt@tallence.com>
2025-06-07 15:46:50 +0200

    add vpc network for gke creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,22 @@
+resource "google_compute_network" "vpc_network" {
+  name                    = "iac-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "iac-subnet"
+  ip_cidr_range = "10.10.0.0/16"
+  region        = var.gcp_region
+  network       = google_compute_network.vpc_network.id
+}
+
 resource "google_container_cluster" "primary" {
   name     = var.gke_cluster_name
   location = var.gcp_region
 
+  network    = google_compute_network.vpc_network.name
+  subnetwork = google_compute_subnetwork.subnet.name
+
   remove_default_node_pool = true
   initial_node_count       = 1
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  name     = "primary-node-pool"
-  cluster  = google_container_cluster.primary.name
-  location = var.gcp_region
-
-  node_config {
-    machine_type = "e2-medium"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-
-  initial_node_count = var.gke_node_count
-}
-
-resource "kubernetes_namespace" "demo" {
-  metadata {
-    name = "demo-namespace"
-    labels = {
-      environment = "ci-cd"
-    }
-  }
 }
