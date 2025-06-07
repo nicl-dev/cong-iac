5ed1b117b4dc047ea14f856a94dd6a0302a45872
niclas.behrendt <niclas.behrendt@tallence.com>
2025-06-07 17:38:13 +0200

    dynamically generate variables from workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,23 @@
 resource "google_compute_network" "vpc_network" {
-  name                    = "iac-vpc"
+  name                    = "cong-vpc-${var.environment}"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name          = "iac-subnet"
+  name          = "cong-subnet-${var.environment}"
   ip_cidr_range = "10.10.0.0/16"
   region        = var.gcp_region
   network       = google_compute_network.vpc_network.id
 }
 
+
 resource "google_container_cluster" "primary" {
-  name     = var.gke_cluster_name
+  name     = "cong-gke-${var.environment}"
   location = var.gcp_region
 
   network    = google_compute_network.vpc_network.name
   subnetwork = google_compute_subnetwork.subnet.name
 
   remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count       = var.gke_node_count
 }
