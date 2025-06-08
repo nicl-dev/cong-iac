9faf2cebdc04e539dd8df540344a670d3cd793f9
niclas.behrendt <niclas.behrendt@tallence.com>
2025-06-08 21:23:55 +0200

    remove gcp provider credentials as we are using workload identity now

--- terraform/modules/gke/providers.tf
+++ terraform/modules/gke/providers.tf
@@ -1,7 +1,6 @@
 provider "google" {
-  credentials = file("gcp-key.json")
-  project     = var.gcp_project
-  region      = var.gcp_region
+  project = var.gcp_project
+  region  = var.gcp_region
 }
 
 data "google_client_config" "default" {}
