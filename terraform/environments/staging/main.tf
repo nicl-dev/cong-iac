2e567111290e1c1c35616e5543719dd8e6cd68df
niclas.behrendt <niclas.behrendt@tallence.com>
2025-06-08 12:00:56 +0200

    create gke module and split terraform into environments

---
+++ terraform/environments/staging/main.tf
@@ -0,0 +1,8 @@
+module "gke" {
+  source           = "../../modules/gke"
+  environment      = "staging"
+  gcp_project      = var.gcp_project
+  gcp_region       = var.gcp_region
+  gke_cluster_name = var.gke_cluster_name
+  gke_node_count   = var.gke_node_count
+}
