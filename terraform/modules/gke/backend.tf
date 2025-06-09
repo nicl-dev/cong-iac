df3adfbadb0b4732627f305a792647182f80db1e
niclas.behrendt <niclas.behrendt@tallence.com>
2025-06-09 12:42:21 +0200

    add backend.tf to gke module as tflint requires it

---
+++ terraform/modules/gke/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.11.0"
+    }
+  }
+}
