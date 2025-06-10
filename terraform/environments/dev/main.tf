87848718b10eb0fd2ed3c9f2f47bcfd4ee8f75cc
niclas.behrendt <niclas.behrendt@tallence.com>
2025-06-10 12:36:25 +0200

    remove namespace from dev for troubleshooting for now

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -4,9 +4,9 @@ module "gke" {
   gcp_project    = "niclas-test"
   gcp_region     = "europe-west1"
   gke_node_count = 1
-}
+# }
 
-module "namespace" {
-  source = "../../modules/k8s"
-  name   = "demo-namespace"
-}
+# module "namespace" {
+#   source = "../../modules/k8s"
+#   name   = "demo-namespace"
+# }
