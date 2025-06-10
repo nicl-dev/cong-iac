a207b5582f53b0893fa6c2d9569f15409a7c8a8d
niclas.behrendt <niclas.behrendt@tallence.com>
2025-06-10 13:00:30 +0200

    read data cluster_ca data from data resource too

--- terraform/environments/dev/providers.tf
+++ terraform/environments/dev/providers.tf
@@ -1,11 +1,3 @@
-provider "kubernetes" {
-  host  = data.google_container_cluster.primary.endpoint
-  token = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(
-    google_container_cluster.primary.master_auth[0].cluster_ca_certificate
-  )
-}
-
 provider "google" {
   project = var.gcp_project
   region  = var.gcp_region
@@ -16,4 +8,12 @@ data "google_client_config" "default" {}
 data "google_container_cluster" "primary" {
   name     = "cong-gke-dev"
   location = var.gcp_region
+}
+
+provider "kubernetes" {
+  host  = data.google_container_cluster.primary.endpoint
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  )
 }
